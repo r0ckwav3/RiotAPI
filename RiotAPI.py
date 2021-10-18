66213955bbd82e8d473530c4b162a992857ef2ad
r0ckwav3 <peterrockwave@gmail.com>
2021-10-17 20:05:24 -0700

    updated for match-v5 API

--- RiotAPI.py
+++ RiotAPI.py
@@ -35,13 +35,17 @@ def exploreDict(d):
 
 
 def gethtml(url, attempts=5):
-    print(url)
+    fp = None
     for j in range(attempts):
         try:
             fp = urllib.request.urlopen(url)
             break
         except Exception as e:
             print("Exception in gethtml:", e)
+
+    if fp is None:
+        print("Error when requesting URL:",url)
+
     mybytes = fp.read()
 
     mystr = mybytes.decode("utf8")
@@ -64,35 +68,35 @@ def getPUUID(name):
     return getSummoner(name)["puuid"]
 
 
-def getMatchIDs(AccountID):  # 0 is the most recent
-    url = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?api_key=%s" % (AccountID,KEY)
+def getMatchIDs(PUUID):  # 0 is the most recent
+    url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/%s/ids?api_key=%s" % (PUUID,KEY)
     temp = json.loads(gethtml(url))
-    return temp["matches"]
+    return temp
 
 
 def getMatch(MatchID):
-    url = "https://na1.api.riotgames.com/lol/match/v4/matches/%s?api_key=%s" % (MatchID,KEY)
+    url = "https://americas.api.riotgames.com/lol/match/v5/matches/%s?api_key=%s" % (MatchID,KEY)
     return json.loads(gethtml(url))
 
 
 def getPlayerNames(Match):
-    identities = Match["participantIdentities"]
-    names = [el["player"]["summonerName"] for el in identities]
+    participants = Match["info"]["participants"]
+    names = [el["summonerName"] for el in participants]
     return names
 
 
-def getParticipantIndex(Match, AccountID):
+def getParticipantIndex(Match, PUUID):
     playerindex = None
     for p in range(10):
-        if Match["participantIdentities"][p]["player"]["accountId"] == AccountID:
+        if Match["info"]["participants"][p]["puuid"] == PUUID:
             playerindex = p
     # print(playerindex,Match["participantIdentities"][playerindex]["player"]["summonerName"])
     if playerindex is None:
-        print("player %s not in match" % AccountID)
+        print("player %s not in match" % PUUID)
         return -1
     else:
         return playerindex
 
 
-def playerWin(Match, AccountID):
-    return Match["participants"][getParticipantIndex(Match, AccountID)]["stats"]["win"]
+def playerWin(Match, PUUID):
+    return Match["info"]["participants"][getParticipantIndex(Match, PUUID)]["win"]
