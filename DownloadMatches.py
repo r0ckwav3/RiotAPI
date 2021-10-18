5cf5744b858db3be517990c2cd99aa0ca84db1b7
r0ckwav3 <peterrockwave@gmail.com>
2021-10-17 21:58:43 -0700

    updated DownloadMatches for match-v5

--- DownloadMatches.py
+++ DownloadMatches.py
@@ -1,27 +1,36 @@
 from RiotAPI import *
 import time
 playername = input("what player: ")
-chosenid = getAccountID(playername)
+chosenid = getPUUID(playername)
 path = "StoredData/%sMatchData.txt" % playername
 f = open(path, mode="w")
 
-# Get the total number of games this player has played.
-tempresponse = gethtml("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?endIndex=0&beginIndex=0&api_key=%s" % (chosenid, KEY))
-totalGames = json.loads(tempresponse)["totalGames"]
-print("This player has played "+str(totalGames)+" games total.")
+# As far as I can tell, there's no equivalet way to get the number of matches
+# in match-v5, but here's the old v4 code that would do that
+# tempresponse = gethtml("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?endIndex=0&beginIndex=0&api_key=%s" % (chosenid, KEY))
+# totalGames = json.loads(tempresponse)["totalGames"]
+# print("This player has played "+str(totalGames)+" games total.")
 
-# matches = getMatchIDs(chosenid)
-ranges = [(i*50, (i+1)*50) for i in range(totalGames//50)]
-# if totalGames is a multiple of 50, this will be length 0, which is fine
-ranges.append(((totalGames//50)*50, totalGames))
+matchchunksize = 64
+currentmatch = 0
 
-for beginIndex, endIndex in ranges:
+while matchchunksize != 0:
     time.sleep(120)
-    matches = json.loads(gethtml("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?endIndex=%s&beginIndex=%s&api_key=%s" % (chosenid, str(endIndex), str(beginIndex), KEY)))["matches"]
-    for j in range(len(matches)):
-        print(j+beginIndex)
-        url = "https://na1.api.riotgames.com/lol/match/v4/matches/%s?api_key=%s" % (matches[j]["gameId"], KEY)
-        matchdata = gethtml(url)
-        f.write(matchdata+"\n")
+    try:
+        matches = getMatchIDs(chosenid, start=currentmatch, count=matchchunksize)
+    except Exception as e:
+        matchchunksize = matchchunksize//2
+        print("getMatchIDs failed, decreasing chunk size to", matchchunksize)
+    else:
+        for j in range(len(matches)):
+            print(j+currentmatch)
+            url = "https://americas.api.riotgames.com/lol/match/v5/matches/%s?api_key=%s" % (matches[j], KEY)
+            try:
+                matchdata = gethtml(url)
+            except Exception as e:
+                pass
+            else:
+                f.write(matchdata+"\n")
+        currentmatch += matchchunksize
 
 f.close()
